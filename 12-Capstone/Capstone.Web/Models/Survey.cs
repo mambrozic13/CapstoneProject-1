19e394abb7c52a367bd225e5418606a9fd6f1ca0
John Brearley <john.brearley1@gmail.com>
2019-07-12 10:54:40 -0400

    fixed celcius, added photo, fixed survey page

--- 12-Capstone/Capstone.Web/Models/Survey.cs
+++ 12-Capstone/Capstone.Web/Models/Survey.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,15 +9,20 @@ namespace Capstone.Web.Models
 {
     public class Survey
     {
-
+        
         public int SurveyId { get; set; }
 
+        [Required]
         public string ParkCode { get; set; }
 
+        [Required]
+        [DataType(DataType.EmailAddress)]
         public string EmailAddress { get; set; }
 
+        [Required]
         public string State { get; set; }
 
+        [Required]
         public string ActivityLevel { get; set; }
     }
 }
